708f74764f218397fffaaca9b35099d14146e369
yeak558 <yeak558@gmail.com>
2026-02-07 13:50:46 +0300

    foundations 08 done, solved in a much different way

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,21 @@
 const removeFromArray = function() {
+	if (arguments.length === 0) { return ;}
+	else if ( arguments.length === 1) { return arguments[1] ;}
+	else {
+		let i = 0;
+		for (arg of arguments) {
+			if (i == 0) {
+				i++;
+				continue;
+			}
+			let index = arguments[0].indexOf(arg);
+			while ( index !== -1 ) {
+				arguments[0].splice(index, 1);
+				index = arguments[0].indexOf(arg);
+			}
+		}
+	}
+	return arguments[0];
 };
 
 // Do not edit below this line
