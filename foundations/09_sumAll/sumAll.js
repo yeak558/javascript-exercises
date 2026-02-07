11625f90855ce28b54701d4decaa5f1707d33629
yeak558 <yeak558@gmail.com>
2026-02-07 14:03:02 +0300

    foundations 09 done

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+	let total = 0;
+	if (! (typeof a === "number" || typeof b === "number")) { return "ERROR"; }
+	if (! (Number.isInteger(a) && Number.isInteger(b)) ) { return "ERROR"; }
+	if (a < 0 || b < 0) { return "ERROR"; }
+	if (a > b) { [a, b] = [b, a]; }
+	for (; a <= b; a++) {
+		total += a;
+	}
+	return total;
 };
 
 // Do not edit below this line
