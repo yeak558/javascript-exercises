a0a96adfb9f7a51049bbf6f91c305272e2108d21
yeak558 <yeak558@gmail.com>
2026-02-18 16:23:52 +0300

    foundations 12 done

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let total = 0;
+  for (el of arr) {
+    total += el;
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+	let result = 1;
+  for (el of arr) {
+    result *= el;
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(base, exp) {
+	let total = 1;
+  for (; exp > 0 ; exp-- ) {
+    total *= base;
+  }
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let result = 1;
+  for (; a > 1; a--) {
+    result *= a;
+  }
+  return result;
 };
 
 // Do not edit below this line
