38935f3001cbcd48cc3bdf7757615407eae81440
yeak558 <yeak558@gmail.com>
2026-02-18 17:14:42 +0300

    foundations 16 done

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+	let peopleWithAge = people.map( function(person) {
+		if ( person.yearOfDeath === undefined )
+		{
+			return (new Date()).getFullYear() - person.yearOfBirth;
+		}
+		else
+		{
+			return ( person.yearOfDeath - person.yearOfBirth );
+		}
+	});
+	let maxAge = Math.max(...peopleWithAge); 	
+	let indexOldest = peopleWithAge.findIndex( age => age === maxAge );
+	return people[indexOldest];
 };
 
 // Do not edit below this line
