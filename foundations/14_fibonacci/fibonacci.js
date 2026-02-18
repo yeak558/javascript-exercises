322c434b429834e0ecb65d732847ccd3070aa689
yeak558 <yeak558@gmail.com>
2026-02-18 16:46:36 +0300

    foundations 14 done

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(order) {
+	if (order < 0) { return "OOPS"; }
+	let total = 1;
+	if (order == 0) {
+		return 0;
+	} if (order == 1 || order == 2) {
+		return 1;
+	} else {
+		return (fibonacci(Number(order) - 1) + fibonacci(Number(order) - 2));
+	}
 };
 
 // Do not edit below this line
