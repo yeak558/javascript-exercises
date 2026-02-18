dc2906ccf8c0d726f6621280aa9b833953873ac5
yeak558 <yeak558@gmail.com>
2026-02-18 16:37:55 +0300

    foundations 13 done

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,30 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+	let endIndex = str.length - 1;
+	let startIndex = 0;
+	str = str.toUpperCase();
+	const skips = [" ", ".", ",", ":", "!", "?"];
+	while (endIndex > str.length / 2 )
+	{
+		if ( skips.includes(str[startIndex]) ) {
+			startIndex++;
+			continue;
+		}
+		if ( skips.includes(str[endIndex]) ) {
+			endIndex--;
+			continue;
+		}
+		if (str[startIndex] !== str[endIndex]) {
+			return false;
+		} else {
+			startIndex++;
+			endIndex--;
+		}
+	}
+	return true;
 };
+
+// console.log(palindromes('racecar')); // true
+// console.log(palindromes('tacos')) // false
 
 // Do not edit below this line
 module.exports = palindromes;
